dd81828e055fbfc3db9aa29c84fb7a349e7b7ad4
iqbalf06 <faisaelz@gmail.com>
2022-11-30 13:37:16 +0700

    first commit

---
+++ database/migration.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"fmt"
+	"waysbucks/models"
+	"waysbucks/pkg/mysql"
+)
+
+// Automatic Migration if Running App
+func RunMigration() {
+	err := mysql.DB.AutoMigrate(
+		&models.User{},
+		&models.Product{},
+		&models.Topping{},
+		&models.Order{},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		panic("Migration Failed")
+	}
+
+	fmt.Println("Migration Success")
+}
